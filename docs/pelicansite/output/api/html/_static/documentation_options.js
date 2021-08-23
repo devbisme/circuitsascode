2f8ceb1060f077043d016d3be486dd682b68c522
devbisme <devb@xess.com>
2021-08-23 16:34:20 -0400

    Fixed web fonts.

--- docs/pelicansite/output/api/html/_static/documentation_options.js
+++ docs/pelicansite/output/api/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.post1.dev12+gc210eaf.d20210823',
+    VERSION: '0.0.post1.dev13+g782afeb.d20210823',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
